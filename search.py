2eaf4602851a76c90a271a4a186a6f8925b5be3b
Matthew <matthewjwest53@gmail.com>
2025-09-14 16:00:57 -0400

    Messy but working generic q1 and q2 solution

--- search.py
+++ search.py
@@ -88,6 +88,96 @@ def tinymaze_search(problem):
     w = Directions.WEST
     return [s, s, w, s, w, w, s, w]
 
+def treeSearch(problem: SearchProblem, strategy, fn):
+    strategy.put((problem.get_start_state(), # State
+                                         [], # Total Path
+                                         0)) # Total Cost 
+    # States that have already been added to fringe
+    visited = list(problem.get_start_state())
+
+    expanded = []
+
+    while True:
+        # No more moves potential moves left
+        if strategy.is_empty():
+            return "Failure"
+        
+        # Visiting next state based on given strategy
+        cur = strategy.get()
+        expanded.append(cur[0])
+        #print("Current node is: ", cur)
+        if (problem.is_goal_state(cur[0])):
+            # Returning the path we took
+            return cur[1]
+        else: # Must continue searching
+            for child in problem.get_successors(cur[0]):
+                # Add all states that haven't been already visited
+                if fn(strategy, child, expanded, visited): #cur[1]:
+                    # Getting path to this node
+                    #print("Child node is: ", child)
+                    path = cur[1][:]
+                    path.append(child[1])
+        
+                    strategy.put((child[0], path, cur[2] + child[2]))
+                    visited.append(child[0])
+
+
+
+def notVisitedBFS(strategy: util.FIFO, child, expanded, visited):
+    if child[0] not in visited:
+        return True
+    else:
+        return False
+    
+def notExpandedDFS(strategy: util.LIFO, child, expanded, visited):
+    if child[0] not in expanded: #Path is list of actions, not list of expanded 
+        return True
+    else:
+        return False
+
+            #Call function that expands fringe for each search type
+            # for child in problem.get_successors(cur[0]):
+            #     # How does this part work for BFS and DFS
+            #     # DFS needs visited to only be when node has been fully expanded
+            #         # Isn't this one right though?
+            #     # BFS needs visited to be when node is on the fringe
+            #     # Add all nodes not already on the fringe
+            #     if child[0] not in cur[3]: # We don't want to 
+            #         # Getting path to this child node
+            #         path = cur[1][:]
+            #         path.append(child[1])
+
+            #         visited = cur[3][:]
+            #         visited.append(cur[0])
+
+            #         #print("Child is: ", (child[0], path, cur[2] + child[2]))
+            #         strategy.put((child[0], path, cur[2] + child[2]))
+            #         #visited.append(child[0])
+    # end while
+
+# def expandFringe(problem: SearchProblem, strategy: util.LIFO, cur, visited):
+#     for child in problem.get_successors(cur[0]):
+#         # Add all states that haven't been already expanded
+#         if child[0] not in expanded:
+#             # Getting path to this node
+#             path = cur[1][:]
+#             path.append(child[1])
+
+#             strategy.put((child[0], path, cur[2] + child[2]))
+#             visited.append(child[0])
+
+# def expandFringe(problem: SearchProblem, strategy: util.FIFO, cur, visited):                    
+#     for child in problem.get_successors(cur[0]):
+#         # Add all states that haven't been already added to fringe
+#         if child[0] not in visited: # Only difference is if condition, can just be a function there??
+#             # Getting path to this node
+#             path = cur[1][:]
+#             path.append(child[1])
+
+#             strategy.put((child[0], path, cur[2] + child[2]))
+#             visited.append(child[0])
+
+
 def dfs(problem: SearchProblem):
     """
     Search the deepest nodes in the search tree first.
@@ -103,17 +193,24 @@ def dfs(problem: SearchProblem):
     print("Start's successors:", problem.get_successors(problem.get_start_state()))
     """
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    
+    return treeSearch(problem, util.LIFO(), notExpandedDFS)
+
 
 def bfs(problem: SearchProblem):
     """Search the shallowest nodes in the search tree first."""
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    return treeSearch(problem, util.FIFO(), notVisitedBFS)
+
+def costFunction(tup):
+    return tup[2]
 
 def ucs(problem: SearchProblem):
     """Search the node of least total cost first."""
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    # Need to pass cost function
+    return treeSearch(problem, util.PriorityQueueWithFunction(costFunction))
+    #util.raiseNotDefined()
 
 def null_heuristic(state, problem=None):
     """
